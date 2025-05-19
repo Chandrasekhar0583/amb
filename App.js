bf8124481a345537687d7dc03cf7a158c192a336
Chandrasekhar0583 <chandumajji4444@gmail.com>
2025-05-19 10:11:05 +0530

    change1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> My first app Open up App.js to start working on your app!</Text>
+      <Text> My first app opne Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
